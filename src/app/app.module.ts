189ae5ff314444a5eeaa4dfb46a7b4ff71016614
pawan kumar <pawankumarms0143@gmail.com>
2020-03-25 15:30:04 +0530

    customer page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { LoginComponent } from './login/login.component';
 import { MainPageBodyComponent } from './main-page-body/main-page-body.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { FooterComponent } from './footer/footer.component';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
   declarations: [
@@ -18,12 +20,14 @@ import { FooterComponent } from './footer/footer.component';
     LoginComponent,
     MainPageBodyComponent,
     SignUpComponent,
-    FooterComponent
+    FooterComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ReactiveFormsModule,
     FormsModule
   ],
   providers: [],
