044ae896fa956f3dba1a59c5d3cc72010a78f1a3
kastu <sharma.kasturi250@gmail.com>
2020-03-28 01:57:20 +0530

    Routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddProductComponent } from './admin/add-product/add-product.component';
+import { AppComponent } from './app.component';
+import { MainPageBodyComponent } from './main-page-body/main-page-body.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin/add-product', component: AddProductComponent},
+  { path: 'main-page', component: MainPageBodyComponent},
+  { path: '', redirectTo: '/main-page', pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
